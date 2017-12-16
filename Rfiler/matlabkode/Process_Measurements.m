258ea763454cc2e8a76094d5abc91fa0bd726c65
banasik <mbanasik@gmail.com>
2017-12-16 16:58:49 +0100

    Blitteratur tilføjet, opdateret Rmetode og Rimplementering er opdateret

--- Rfiler/matlabkode/Process_Measurements.m
+++ Rfiler/matlabkode/Process_Measurements.m
@@ -18,5 +18,36 @@ BIsignal = filter(lpFilt,BIabs); %BI signalet filteret
 % Detrending data
 [p,s,mu] = polyfit((1:numel(BIsignal))',BIsignal,6);
 f_y = polyval(p,(1:numel(BIsignal))',[],mu);
+BI_data = BIsignal - f_y; 
 
+% Den indtastet stroem
+strom = str2double(get(handles.txtstrom,'String'));
+BI_dataImpedans = (BI_data/(strom*10^-5));
+
+% Downsampler BI
+downBI = downsample(BI_dataImpedans,20000);
+% Smoother BI
+smoothBI = smooth(downBI);
+
+% Detekterer synk (peaks)
+BI_inverted = -smoothBI;
+[~,locs_synk] = findpeaks(BI_inverted,'MinPeakHeight',4.5,...
+                                       'MinPeakDistance',30);
+handles.locs_synk = locs_synk;
+
+handles.BIsignal = smoothBI; %BI signalet gemmes i handles til senere visning
+
+
+%% EMG
+
+% Downsampler EMG
+downEMG = downsample(handles.EMG,20000);
+% Smoother EMG
+smoothEMG = smooth(downEMG);
+
+handles.EMGsignal = smoothEMG; %EMG signalet gemmes i handles til senere visning
+%% TID
+% Downsampler EMG
+handles.TID = downsample(handles.timestamps,20000); % Nu har tid samme stoerrelse som BI og EMG
+end
 
